65934183bdef342c8094ef5ba5ad155c98c92040
Thato Metsing <thatometsingiq@gmail.com>
2025-06-02 21:13:36 +0200

    Add files via upload

---
+++ WindowsApp/WindowsApp1/WindowsApp1/Form1.Designer.vb
@@ -0,0 +1,180 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtLength = New System.Windows.Forms.ComboBox()
+        Me.txtRollCoverage = New System.Windows.Forms.ComboBox()
+        Me.txtHeight = New System.Windows.Forms.ComboBox()
+        Me.txtWidth = New System.Windows.Forms.ComboBox()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.txtSingleRoll = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txtLength
+        '
+        Me.txtLength.FormattingEnabled = True
+        Me.txtLength.Items.AddRange(New Object() {"10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35"})
+        Me.txtLength.Location = New System.Drawing.Point(234, 64)
+        Me.txtLength.Name = "txtLength"
+        Me.txtLength.Size = New System.Drawing.Size(121, 21)
+        Me.txtLength.TabIndex = 0
+        '
+        'txtRollCoverage
+        '
+        Me.txtRollCoverage.FormattingEnabled = True
+        Me.txtRollCoverage.Items.AddRange(New Object() {"40", "40.5", "41", "41.5", "42", "42.5", "43", "43.5", "44", "44.5", "45", "45.5", "46", "46.5", "47", "47.5", "48", "48.5", "49", "49.5", "50"})
+        Me.txtRollCoverage.Location = New System.Drawing.Point(234, 244)
+        Me.txtRollCoverage.Name = "txtRollCoverage"
+        Me.txtRollCoverage.Size = New System.Drawing.Size(121, 21)
+        Me.txtRollCoverage.TabIndex = 1
+        '
+        'txtHeight
+        '
+        Me.txtHeight.FormattingEnabled = True
+        Me.txtHeight.Items.AddRange(New Object() {"10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35"})
+        Me.txtHeight.Location = New System.Drawing.Point(234, 187)
+        Me.txtHeight.Name = "txtHeight"
+        Me.txtHeight.Size = New System.Drawing.Size(121, 21)
+        Me.txtHeight.TabIndex = 2
+        '
+        'txtWidth
+        '
+        Me.txtWidth.FormattingEnabled = True
+        Me.txtWidth.Items.AddRange(New Object() {"10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35"})
+        Me.txtWidth.Location = New System.Drawing.Point(234, 128)
+        Me.txtWidth.Name = "txtWidth"
+        Me.txtWidth.Size = New System.Drawing.Size(121, 21)
+        Me.txtWidth.TabIndex = 3
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Location = New System.Drawing.Point(422, 179)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(75, 23)
+        Me.btnCalculate.TabIndex = 4
+        Me.btnCalculate.Text = "Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(503, 180)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(68, 23)
+        Me.btnExit.TabIndex = 5
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'txtSingleRoll
+        '
+        Me.txtSingleRoll.Location = New System.Drawing.Point(422, 144)
+        Me.txtSingleRoll.Name = "txtSingleRoll"
+        Me.txtSingleRoll.Size = New System.Drawing.Size(149, 20)
+        Me.txtSingleRoll.TabIndex = 6
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(119, 67)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(70, 13)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "Length (feet);"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(119, 131)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(65, 13)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "Width (feet):"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(119, 190)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(68, 13)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "Height (feet):"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(119, 247)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(109, 13)
+        Me.Label4.TabIndex = 10
+        Me.Label4.Text = "Roll Coverage (sqrFt):"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(419, 128)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(60, 13)
+        Me.Label5.TabIndex = 11
+        Me.Label5.Text = "Single Roll:"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtSingleRoll)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.txtWidth)
+        Me.Controls.Add(Me.txtHeight)
+        Me.Controls.Add(Me.txtRollCoverage)
+        Me.Controls.Add(Me.txtLength)
+        Me.Name = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtLength As ComboBox
+    Friend WithEvents txtRollCoverage As ComboBox
+    Friend WithEvents txtHeight As ComboBox
+    Friend WithEvents txtWidth As ComboBox
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents txtSingleRoll As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+End Class
---
+++ WindowsApp/WindowsApp1/WindowsApp1/Form1.vb
@@ -0,0 +1,21 @@
+﻿Public Class Form1
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+
+        Dim length, width, height, surfaceArea, rollCoverage, singleRoll As Double
+
+        length = Val(txtLength.Text)
+        width = Val(txtWidth.Text)
+        height = Val(txtHeight.Text)
+        surfaceArea = 2 * ((length * height) + (width * height))
+        rollCoverage = Val(txtRollCoverage.Text)
+        singleRoll = Math.Ceiling(Val(surfaceArea / rollCoverage))
+        txtSingleRoll.Text = singleRoll
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+
+        Application.Exit()
+
+    End Sub
+End Class
